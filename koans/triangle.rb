04af466027b423d45c597959be6523523c95e885
mikeebert <mike@mikeebert.net>
2011-12-06 18:43:23 -0600

    164 koans

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if (a == 0 or b == 0 or c == 0)
+    raise TriangleError
+  end
+  
+  if a < 0 || b < 0 or c < 0
+    raise TriangleError
+  end
+    
+  if (a + b <= c) || (a + c <= b) || (c + b <= a)
+    raise TriangleError
+  end
+
+  # if (a + b =< c) || (a + c < b) || (c + b < a)
+  #   raise TriangleError
+  # end
+  
   if (a == b and a == c)
     value = :equilateral
   else 
